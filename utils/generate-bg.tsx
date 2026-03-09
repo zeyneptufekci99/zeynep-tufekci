d6b23b0db5c02a7f11a135cba318e60a4082a3a8
zeyneptufekci99 <93983517+zeyneptufekci99@users.noreply.github.com>
2026-03-09 15:41:59 +0300

    fix: fix console error

--- utils/generate-bg.tsx
+++ utils/generate-bg.tsx
@@ -1,22 +1,18 @@
 import { colors } from "@/styles/colors";
 
 type GenerateProps = {
-  max?: number;
-  min?: number;
   degree?: number;
   from?: string;
   to?: string;
 };
 
-export const generateBG = (props: GenerateProps) => {
+export const generateBG = (props: GenerateProps = {}) => {
   const fromColor = props.from ?? `${colors.primary.DEFAULT} 0%`;
   const toColor = props.to ?? `${colors.black} 100%`;
-  const maxDegree = props.max ?? 360;
-  const minDegree = props.min ?? 0;
-  const computedDegree =
-    Math.floor(Math.random() * (maxDegree - minDegree + 1)) + minDegree;
+
+  const degree = props.degree ?? 195;
 
   return {
-    backgroundImage: `linear-gradient(${props.degree ?? computedDegree}deg, ${fromColor}, ${toColor})`,
+    backgroundImage: `linear-gradient(${degree}deg, ${fromColor}, ${toColor})`,
   } as const;
 };
