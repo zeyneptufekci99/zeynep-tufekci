35b1e282bd5b7bd3e7eadceffeb4e614778df9c7
zeyneptufekci99 <93983517+zeyneptufekci99@users.noreply.github.com>
2026-03-10 19:36:21 +0300

    fix: fix hero height

--- components/custom/hero.tsx
+++ components/custom/hero.tsx
@@ -17,7 +17,7 @@ export const Hero = ({ onClickConnect, onClickProjects }: IHero) => {
   };
 
   return (
-    <section className="w-full relative min-h-screen flex items-center justify-center bg-black overflow-hidden">
+    <section className="w-full relative h-[calc(100vh-100px)]  flex items-center justify-center bg-black overflow-hidden">
       <div className="absolute inset-0 bg-[linear-gradient(195deg,rgba(89,22,139,1)_0%,rgba(0,0,0,1)_100%)] opacity-60" />
 
       <div className="relative z-10 text-center">
